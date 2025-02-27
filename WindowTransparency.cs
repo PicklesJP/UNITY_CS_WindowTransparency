2e5d9d1661fbf518bed6c4b5b719bc64d92cae96
Pickles.JP <tt19786741hiyokoprogramme@gmail.com>
2025-02-27 20:05:17 +0900

    Update WindowTransparency.cs
    可読性の排除と説明を追加
    コメントを消した場合の文字数を710文字に変更

--- WindowTransparency.cs
+++ WindowTransparency.cs
@@ -1,18 +1,16 @@
-using UnityEngine;
-using System;
-using System.Runtime.InteropServices;
-public class WindowTransparency : MonoBehaviour
+using UnityEngine; using System; using System.Runtime.InteropServices; //可読性は考慮せずに行数を極限まで減らしていますコメントを消した場合の文字数は710文字です
+public class WindowTransparency : MonoBehaviour //名前を指定しています
 {
     [DllImport("User32.dll")] static extern IntPtr GetActiveWindow(); //Windows APIのDLLインポート
-    [DllImport("User32.dll")] static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-    [DllImport("user32.dll")] static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-    [DllImport("user32.dll")] static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
-    void Awake(){ //デフォルトの装飾子がprivateのため省略
-#if !UNITY_EDITOR //.exeに出力した時のみ機能
-        IntPtr window = GetActiveWindow();
-        SetWindowLong(window, -20, 0x00080000); //ウィンドウ設定
-        SetWindowLong(window, -16, GetWindowLong(window, -16) ^ 0x00C00000); //タイトルバー削除
-        SetLayeredWindowAttributes(window, 0, 0, 1); //ウィンドウを透過
+    [DllImport("User32.dll")] static extern int SetWindowLong(IntPtr h, int i, int v); //h=hWnd,i=nIndex,v=dwNewLong,c=crKey,b=bAlpha,f=dwFlagsとして省略
+    [DllImport("user32.dll")] static extern int GetWindowLong(IntPtr h, int i); //上記同じ
+    [DllImport("user32.dll")] static extern bool SetLayeredWindowAttributes(IntPtr h, uint c, byte b, uint f); //上記同じ
+    void Awake(){ //デフォルトの装飾子がprivateのため省略,上記同理由
+#if !UNITY_EDITOR //.exeに出力した時のみ機能する,エディターの動作が壊れます
+        IntPtr h = GetActiveWindow(); //単語を=にして省略
+        SetWindowLong(h, -20, 0x80000); //ウィンドウ設定,0x00080000は=0x80000なので省略
+        SetWindowLong(h, -16, GetWindowLong(h, -16) ^ 0xC00000); //タイトルバー削除,上記と同じ理由で省略
+        SetLayeredWindowAttributes(h, 0, 0, 1); //ウィンドウを透過,=にした単語から代入
 #endif
     }
 }
