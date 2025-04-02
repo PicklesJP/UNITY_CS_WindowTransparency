f3af767cef98c59a0cbe3fd9fdba4da05e5e6ddc
Pickles.JP <tt19786741hiyokoprogramme@gmail.com>
2025-04-03 01:14:33 +0900

    Update WindowTransparency.cs

--- WindowTransparency.cs
+++ WindowTransparency.cs
@@ -1,16 +1,16 @@
-using UnityEngine; using System; using System.Runtime.InteropServices; //可読性は考慮せずに行数を極限まで減らしていますコメントを消した場合の文字数は710文字です
-public class WindowTransparency : MonoBehaviour //名前を指定しています
+using UnityEngine; using System; using System.Runtime.InteropServices;
+public class WindowTransparency : MonoBehaviour
 {
-    [DllImport("User32.dll")] static extern IntPtr GetActiveWindow(); //Windows APIのDLLインポート
-    [DllImport("User32.dll")] static extern int SetWindowLong(IntPtr h, int i, int v); //h=hWnd,i=nIndex,v=dwNewLong,c=crKey,b=bAlpha,f=dwFlagsとして省略
-    [DllImport("user32.dll")] static extern int GetWindowLong(IntPtr h, int i); //上記同じ
-    [DllImport("user32.dll")] static extern bool SetLayeredWindowAttributes(IntPtr h, uint c, byte b, uint f); //上記同じ
-    void Awake(){ //デフォルトの装飾子がprivateのため省略,上記同理由
-#if !UNITY_EDITOR //.exeに出力した時のみ機能する,エディターの動作が壊れます
-        IntPtr h = GetActiveWindow(); //単語を=にして省略
-        SetWindowLong(h, -20, 0x80000); //ウィンドウ設定,0x00080000は=0x80000なので省略
-        SetWindowLong(h, -16, GetWindowLong(h, -16) ^ 0xC00000); //タイトルバー削除,上記と同じ理由で省略
-        SetLayeredWindowAttributes(h, 0, 0, 1); //ウィンドウを透過,=にした単語から代入
+    [DllImport("User32.dll")] static extern IntPtr GetActiveWindow(); //WindowsAPI DLL
+    [DllImport("User32.dll")] static extern int SetWindowLong(IntPtr h, int i, int v); //h=hWnd,i=nIndex,v=dwNewLong,c=crKey,b=bAlpha,f=dwFlags
+    [DllImport("user32.dll")] static extern int GetWindowLong(IntPtr h, int i);
+    [DllImport("user32.dll")] static extern bool SetLayeredWindowAttributes(IntPtr h, uint c, byte b, uint f);
+    void Awake(){ //default private
+#if !UNITY_EDITOR //.exe only work, not work in UnityEditor
+        IntPtr h = GetActiveWindow();
+        SetWindowLong(h, -20, 0x80000);
+        SetWindowLong(h, -16, GetWindowLong(h, -16) ^ 0xC00000);
+        SetLayeredWindowAttributes(h, 0, 0, 1); //代入
 #endif
     }
 }
